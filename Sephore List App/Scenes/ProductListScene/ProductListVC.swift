ffddd1a10b16ae978f1f352b19a1ecef414f7f69
mossab12 <fm_djeradi@esi.dz>
2022-06-06 00:34:19 +0100

    setting up the ProductList Scene (View and ViewController)

---
+++ Sephore List App/Scenes/ProductListScene/ProductListVC.swift
@@ -0,0 +1,12 @@
+//
+//  ProductListVC.swift
+//  Sephore List App
+//
+//  Created by mossab on 6/6/2022.
+//
+
+import UIKit
+
+class ProductListVC: PairingViewController<ProductListView> {
+    
+}
---
+++ Sephore List App/Scenes/ProductListScene/ProductListView.swift
@@ -0,0 +1,15 @@
+//
+//  ProductListView.swift
+//  Sephore List App
+//
+//  Created by mossab on 6/6/2022.
+//
+
+import UIKit
+
+class ProductListView: UIView {
+    
+    
+    
+    
+}
